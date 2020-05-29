48b339a60ca2591bafd557355f09c19f26524941
eTrinityE <contaduduzi@gmail.com>
2020-05-29 20:15:40 -0300

    Menu repetindo

--- src/avaliacao.java
+++ src/avaliacao.java
@@ -6,10 +6,12 @@ public class avaliacao {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int horastrab, valorhr;
+		int horastrab, valorhr, custototal, custoparcial, totalhrs, opcaomenu;
 		char escolha;
 		String nome;
 		
+		
+		totalhrs = 0;
 		
 		System.out.println("Nome: ");
 		nome = sc.next();
@@ -23,6 +25,13 @@ public class avaliacao {
 		System.out.println("Digitar outro (S/N)?");
 		escolha = sc.next().charAt(0);
 		
+		custoparcial = horastrab * valorhr;
+		custototal = 0;
+		custototal = custototal + custoparcial;
+		
+		totalhrs = totalhrs + horastrab;
+		
+	
 		while (escolha == 'S') {
 			System.out.println("Nome: ");
 			nome = sc.next();
@@ -35,7 +44,31 @@ public class avaliacao {
 			
 			System.out.println("Digitar outro (S/N)?");
 			escolha = sc.next().charAt(0);
+			
+			custoparcial = horastrab * valorhr;
+			custototal = custototal + custoparcial;
+			
+			totalhrs = totalhrs + horastrab;
 		}
+		
+		System.out.println("MENU");
+		System.out.println("1 - Total de horas trabalhadas");
+		System.out.println("2 - Custo total");
+		System.out.println("3 - Nome da pessoa que ganhou mais");
+		System.out.println("4 - Sair");
+		System.out.println("Digite uma opcão: ");
+		opcaomenu = sc.nextInt();
+		
+		while (opcaomenu != 4) {
+			System.out.println("MENU");
+			System.out.println("1 - Total de horas trabalhadas");
+			System.out.println("2 - Custo total");
+			System.out.println("3 - Nome da pessoa que ganhou mais");
+			System.out.println("4 - Sair");
+			System.out.println("Digite uma opcão: ");
+			opcaomenu = sc.nextInt();
+		}
+		
 		
 	}
 
